07d75899a853ea7131d8b9f43c8b6207ed5593e8
Ryvan Crazinov <ryvan@Crazys-MacBook.local>
2017-10-03 09:07:14 -0600

    added login html

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,62 @@
 <template>
 	<div id="login">
-		<h2>Subtitle</h2>
-		<h2 class="subtitle">Bulma Subtitle</h2>
-		<button>Normal</button>
-		<button class="button is-primary">Bulma Button</button>
+		<div class="columns is-half-desktop">
+			<div class="column">&nbsp;</div>
+			<div class="column">
+				<h2 class="title">Login</h2>
+				<div class="field">
+				  <p class="control has-icons-left has-icons-right">
+				    <input class="input" type="email" placeholder="Email" v-model="email">
+				    <span class="icon is-small is-left">
+				      <i class="fa fa-envelope"></i>
+				    </span>
+				    <span class="icon is-small is-right">
+				      <i class="fa fa-check"></i>
+				    </span>
+				  </p>
+				</div>
+				<div class="field">
+				  <p class="control has-icons-left">
+				    <input class="input" type="password" placeholder="Password" v-model="password">
+				    <span class="icon is-small is-left">
+				      <i class="fa fa-lock"></i>
+				    </span>
+				  </p>
+				</div>
+				<div class="field">
+				  <p class="control">
+				    <button class="button is-primary" v-on:click="submitLogin">
+				      Login
+				    </button>
+				  </p>
+				</div>
+			</div>
+			<div class="column">&nbsp;</div>
+		</div>
 	</div>
 </template>
 <script>
+	import axios from 'axios';
 	export default {
-		name: 'login'
+		name: 'login',
+		data: () => ({
+			email: '',
+			password: ''
+		}),
+		methods: {
+			submitLogin(){
+				axios.post('https://localhost:3005/v1/account/login', {
+				    email: this.email,
+				    password: this.password
+				  })
+				  .then(function (response) {
+				    console.log(response);
+				  })
+				  .catch(function (error) {
+				    console.log(error);
+				  });
+			}
+		}
 	}
 </script>
 <style>
